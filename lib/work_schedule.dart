70e06cae8058bef2406ef14a43499bf8bdfbb062
Dude147 <dumeni.manatschal@gmail.com>
2023-09-15 14:19:49 +0200

    include schedule widget

---
+++ lib/work_schedule.dart
@@ -0,0 +1,23 @@
+// import 'animated_toggle.dart';
+import 'package:flutter/material.dart';
+import 'package:scheduler/context.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:scheduler/work_toggler.dart';
+
+/// Flutter code sample for [IconButton].
+
+class WorkSchedule extends StatefulWidget {
+  final GlobalContext _globalContext;
+
+  WorkSchedule(this._globalContext);
+
+  @override
+  State<WorkSchedule> createState() => _WorkSchedule();
+}
+
+class _WorkSchedule extends State<WorkSchedule> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
