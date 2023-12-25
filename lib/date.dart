b803df6e250bf383f7370e1adca15eecca634c18
Dude19735 <dumeni.manatschal@gmail.com>
2023-12-26 00:16:47 +0100

    this happens if you disable auto format and then, at some point, enable it again...

--- lib/date.dart
+++ lib/date.dart
@@ -10,8 +10,34 @@ class Date {
   late int _month;
   late int _day;
 
-  static const List<int> _dpm = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  static const List<int> _dpmLpY = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  static const List<int> _dpm = [
+    31,
+    28,
+    31,
+    30,
+    31,
+    30,
+    31,
+    31,
+    30,
+    31,
+    30,
+    31
+  ];
+  static const List<int> _dpmLpY = [
+    31,
+    29,
+    31,
+    30,
+    31,
+    30,
+    31,
+    31,
+    30,
+    31,
+    30,
+    31
+  ];
 
   static DateStyle getDateStyle(double width, TextStyle textStyle) {
     DateStyle res = DateStyle.full;
@@ -67,6 +93,11 @@ class Date {
     return res;
   }
 
+  @override
+  String toString() {
+    return Date.Date2Str(this);
+  }
+
   static String Date2Str(Date day, {DateStyle style = DateStyle.full}) {
     // TODO: do some locale stuff here
     assert(style == DateStyle.full ||
@@ -86,7 +117,7 @@ class Date {
     }
     return res;
   }
-  
+
   Date.today() {
     DateTime now = DateTime.now();
     _year = now.year;
@@ -94,7 +125,7 @@ class Date {
     _day = now.day;
   }
 
-  Date.fromDateTime(DateTime date){
+  Date.fromDateTime(DateTime date) {
     _year = date.year;
     _month = date.month;
     _day = date.day;
@@ -105,7 +136,7 @@ class Date {
     _year = now.year;
     _month = now.month;
     _day = now.day;
-    selfSubtractDays(now.weekday-1);
+    selfSubtractDays(now.weekday - 1);
   }
 
   // Date.nextSunday() {
@@ -118,7 +149,7 @@ class Date {
   //   print(toFormatedString());
   // }
 
-  Date.fromInt(int date){
+  Date.fromInt(int date) {
     _year = date ~/ 10000;
     _month = (date - _year * 10000) ~/ 100;
     _day = (date - _year * 10000 - _month * 100).round();
@@ -132,35 +163,39 @@ class Date {
   bool isLeap() => _isLeap();
   int weekday() => DateTime(_year, _month, _day).weekday;
 
-  DateTime toDateTime(){
+  DateTime toDateTime() {
     return DateTime(_year, _month, _day);
   }
 
-  int compareTo(Date date){
-    if(date.year() == _year && date.month() == _month && date.day() == _day) return 0;
-    if(date.year() < _year) return 1;
-    if(date.year() == _year && date.month() < _month) return 1;
-    if(date.year() == _year && date.month() == _month && date.day() < _day) return 1;
+  int compareTo(Date date) {
+    if (date.year() == _year && date.month() == _month && date.day() == _day)
+      return 0;
+    if (date.year() < _year) return 1;
+    if (date.year() == _year && date.month() < _month) return 1;
+    if (date.year() == _year && date.month() == _month && date.day() < _day)
+      return 1;
     return -1;
   }
 
-  String toFormatedString(){
+  String toFormatedString() {
     return "${_day.toString().padLeft(2, '0')}.${_month.toString().padLeft(2, '0')}.$_year";
   }
 
-  int absWindowSizeWith(Date date){
+  int absWindowSizeWith(Date date) {
+    // this works because our dates don't have minutes and seconds
     return toDateTime().difference(date.toDateTime()).inDays.abs() + 1;
   }
 
-  int absDiff(Date date){
+  int absDiff(Date date) {
+    // this works because our dates don't have minutes and seconds
     return toDateTime().difference(date.toDateTime()).inDays.abs();
   }
 
-  int toInt(){
+  int toInt() {
     return (_year * 10000 + _month * 100 + _day).round();
   }
 
-  int toShortInt(){
+  int toShortInt() {
     if (_year < 2000 || _year > 2099) {
       throw Exception(
           "Out-of-bounds: [$_year] Years must be in [1000, 9999] range!");
@@ -168,99 +203,98 @@ class Date {
     return ((_year - 2000) * 10000 + _month * 100 + _day).round();
   }
 
-  void _addOneDay(List<int> dpm){
+  void _addOneDay(List<int> dpm) {
     _day++;
-    if(_day > dpm[_month-1]){
+    if (_day > dpm[_month - 1]) {
       _day = 1;
       _month++;
     }
-    if(_month>12){
+    if (_month > 12) {
       _year++;
-      _month=1;
+      _month = 1;
     }
   }
 
-  void _subOneDay(List<int> dpm){
+  void _subOneDay(List<int> dpm) {
     _day--;
-    if(_day < 1){
+    if (_day < 1) {
       _month--;
     }
-    if(_month<1){
+    if (_month < 1) {
       _year--;
-      _month=12;
+      _month = 12;
     }
-    if(_day < 1){
-      _day = dpm[_month-1];
+    if (_day < 1) {
+      _day = dpm[_month - 1];
     }
   }
 
-  void selfSubtractDays(int days){
-    while(days > 0){
+  void selfSubtractDays(int days) {
+    while (days > 0) {
       _subOneDay(_isLeap() ? _dpmLpY : _dpm);
       days--;
     }
   }
 
-  Date subtractDays(int days){
+  Date subtractDays(int days) {
     var temp = Date(_year, _month, _day);
 
-    while(days > 0){
+    while (days > 0) {
       temp._subOneDay(temp._isLeap() ? _dpmLpY : _dpm);
       days--;
     }
     return temp;
   }
 
-  void selfAddDays(int days){
-    while(days > 0){
+  void selfAddDays(int days) {
+    while (days > 0) {
       _addOneDay(_isLeap() ? _dpmLpY : _dpm);
       days--;
     }
   }
 
-  Date addDays(int days){
+  Date addDays(int days) {
     var temp = Date(_year, _month, _day);
 
-    while(days > 0){
+    while (days > 0) {
       temp._addOneDay(temp._isLeap() ? _dpmLpY : _dpm);
       days--;
     }
     return temp;
   }
 
-  bool _isLeap(){
+  bool _isLeap() {
     return _year % 4 == 0 || (!(_year % 100 == 0) && _year % 400 == 0);
   }
 
-  _assert(){
+  _assert() {
     if (_year < 1000 || _year > 9999) {
       throw Exception(
           "Out-of-bounds: [$_year] Years must be in [1000, 9999] range!");
     }
 
     if (_month < 1 || _month > 12) {
-      throw Exception("Out-of-bounds: [$_month] Month must be in [1, 12] range!");
+      throw Exception(
+          "Out-of-bounds: [$_month] Month must be in [1, 12] range!");
     }
 
-    if(_month != 2){
-      if(_isLeap()){
-        if(_day < 1 || _day > _dpmLpY[_month-1]){
-          throw Exception("Out-of-bounds: [$_day] Days must be in [1,${_dpmLpY[_month-1]}] for a leap year!");
+    if (_month != 2) {
+      if (_isLeap()) {
+        if (_day < 1 || _day > _dpmLpY[_month - 1]) {
+          throw Exception(
+              "Out-of-bounds: [$_day] Days must be in [1,${_dpmLpY[_month - 1]}] for a leap year!");
+        }
+      } else {
+        if (_day < 1 || _day > _dpm[_month - 1]) {
+          throw Exception(
+              "Out-of-bounds: [$_day] Days must be greater in [1,${_dpmLpY[_month - 1]}] for a regular year!");
         }
       }
-      else{
-        if(_day < 1 || _day > _dpm[_month-1]){
-          throw Exception("Out-of-bounds: [$_day] Days must be greater in [1,${_dpmLpY[_month-1]}] for a regular year!");
-        }
-      }
-    }
-    else {
-      int d = _isLeap()
-          ? 29
-          : 28;
+    } else {
+      int d = _isLeap() ? 29 : 28;
       if (_day > d) {
         throw Exception(
-            "Out-of-bounds: [$_day] Days must be in [1, $d] range for a February in a ${d==28 ? "regular" : "leap"} year!");
+            "Out-of-bounds: [$_day] Days must be in [1, $d] range for a February in a ${d == 28 ? "regular" : "leap"} year!");
       }
     }
   }
